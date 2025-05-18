254ef5cf2e64c6be701e3b489018e2fcf1e366a9
sandeep <sandeep.k.mishra8@gmail.com>
2025-05-19 00:33:23 +0530

    initial commi -- second committ
    	modified:   vector-db-demo/src/main/java/org/vectordb/demo/TestMain.java

--- vector-db-demo/src/main/java/org/vectordb/demo/TestMain.java
+++ vector-db-demo/src/main/java/org/vectordb/demo/TestMain.java
@@ -9,6 +9,7 @@ public class TestMain {
     public static String chatGPT(String prompt) {
         String url = "https://api.openai.com/v1/chat/completions";
         String model = "gpt-3.5-turbo";
+        
 
         try {
             URL obj = new URL(url);
